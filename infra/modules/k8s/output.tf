17c3ab07e25be91adcb8a2fdd11fa531b00b8c7f
Ashutosh Rathore <ashutoshrathore@Ashutoshs-MacBook-Air.local>
2024-10-29 21:26:42 +0200

    more fixes

--- infra/modules/k8s/output.tf
+++ infra/modules/k8s/output.tf
@@ -14,16 +14,17 @@ output "host" {
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.client_key
+  value = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key : null
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.client_certificate
+  value = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate : null
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.cluster_ca_certificate
+  value = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate : null
 }
+
 
 output "kube_admin_config_raw" {
   value = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
