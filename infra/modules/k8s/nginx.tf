862ac2391fcda28135412bc97402d6927dafc97e
Ashutosh Rathore <ashutoshrathore@Ashutoshs-MacBook-Air.local>
2024-10-29 22:55:16 +0200

    more changes

--- infra/modules/k8s/nginx.tf
+++ infra/modules/k8s/nginx.tf
@@ -1,5 +1,4 @@
 resource "helm_release" "nginx_ingress" {
-  depends_on = [ azurerm_kubernetes_cluster.aks ]
   name       = "nginx-ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
